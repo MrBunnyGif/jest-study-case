f4bea4686a8e4c7257643098bf693713efd75d76
Pedro Botega <botegapedro@gmail.com>
2024-06-11 20:40:14 +0100

    Mock jest function test 1

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,12 @@
-test("mock implementation of a basic function", () => {
-    const mock = jest.fn(x => x + 42)
-    expect(mock(1)).toBe(43)
-    expect(mock).toHaveBeenCalledWith(1)
+test("spying on a method of an object", () => {
+    const video = {
+        play() {
+            return true
+        }
+    }
+
+    const spy = jest.spyOn(video, "play")
+    video.play()
+    expect(spy).toHaveBeenCalled()
+    spy.mockRestore()
 })
