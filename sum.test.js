c0a4c5647b0bfd76dc6b960b28339f3af871e3cb
Pedro Botega <botegapedro@gmail.com>
2024-06-11 20:34:36 +0100

    Mock jest function test

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,5 @@
-const fetchPromise = require("./sum")
-
-test("data is peanut", async () => {
-    const data = await fetchPromise()
-    expect(data).toBe("peanut")
+test("mock implementation of a basic function", () => {
+    const mock = jest.fn(x => x + 42)
+    expect(mock(1)).toBe(43)
+    expect(mock).toHaveBeenCalledWith(1)
 })
